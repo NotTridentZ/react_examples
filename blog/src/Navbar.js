59f6d360a49344e68bea846ca7e083fb6948533e
Andre <andreputra.ap@gmail.com>
2024-07-22 15:53:55 +0700

    init

---
+++ blog/src/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <h1>Blog</h1>
+        <div>
+            <a href="">Home</a>
+            <a href="">New Blog</a>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
